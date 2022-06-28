3f7e6a77319b479da0cc8dd426d7b11ddcac799f
Irina Komarova <kmrva.is@gmail.com>
2022-06-28 15:49:39 +0300

    init: start

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'b3523ec7116948a7a3546bfdd233ce0e', // получите свой ключ https://newsapi.org/
         });
     }
 }
